51e889a80749c63f1a2ee89ef4002c3e5dd3c1f9
egjallow10 <egjallow10@gmail.com>
2023-04-09 05:49:10 +0000

    fans aggregation

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,10 @@
----sscript that ranks country origins of bands, 
---- ordered by the number of (non-unique) fans
+-- sscript that ranks country origins of bands, 
+--  ordered by the number of (non-unique) fans
 
 
 SELECT 
   origin, 
-  SUM(fans) AS nb_fans, 
+  SUM(fans) AS nb_fans 
 FROM 
   metal_bands
 GROUP BY 
