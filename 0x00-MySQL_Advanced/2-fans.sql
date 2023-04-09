d56452115ae797345aabebef91c5e668d89924a3
egjallow10 <egjallow10@gmail.com>
2023-04-09 01:36:09 +0000

    country origin

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,7 +2,7 @@
 --- ordered by the number of (non-unique) fans
 
 
-SSELECT origin, SUM(fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
     ORDER BY nb_fans DESC;
